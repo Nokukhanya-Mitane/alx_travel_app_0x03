9eca41b13940b2d9120e95f271db1814e365d165
Nokukhanya-Mitane <nokukhanyamitane@gmail.com>
2026-01-11 17:08:39 +0200

    Update settings.py

--- alx_travel_app/listings/settings.py
+++ alx_travel_app/listings/settings.py
@@ -1,13 +1,38 @@
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = 'dummy-secret-key'
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
 INSTALLED_APPS = [
-    ...
-    'django_celery_results',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
     'listings',
+    'django_celery_results',
 ]
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-DEFAULT_FROM_EMAIL = 'no-reply@alxtravel.com'
 
 CELERY_BROKER_URL = 'amqp://localhost'
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_BACKEND = 'django-db'
+
+STATIC_URL = '/static/'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
