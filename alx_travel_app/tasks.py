087303c30a5bf0fff0d32a89bf211456e4973b07
Nokukhanya-Mitane <nokukhanyamitane@gmail.com>
2026-01-11 14:26:34 +0200

    Create tasks.py

---
+++ alx_travel_app/tasks.py
@@ -0,0 +1,15 @@
+from celery import shared_task
+from django.core.mail import send_mail
+
+
+@shared_task
+def send_booking_confirmation_email(email, booking_id):
+    subject = "Booking Confirmation"
+    message = f"Your booking with ID {booking_id} has been confirmed."
+    send_mail(
+        subject,
+        message,
+        None,
+        [email],
+        fail_silently=False,
+    )
