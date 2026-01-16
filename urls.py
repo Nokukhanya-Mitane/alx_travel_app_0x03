c270891a8843a0b2e0cc162d3c7fda5bd7eafecf
Nokukhanya-Mitane <nokukhanyamitane@gmail.com>
2026-01-16 18:10:07 +0200

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,6 +1,20 @@
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
 from listings.views import initiate_payment, verify_payment
 
-urlpatterns = [
+schema_view = get_schema_view(
+   openapi.Info(
+      title="ALX Travel API",
+      default_version='v1',
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
+
+urlpatterns += [
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0)),
     path("api/initiate-payment/", initiate_payment),
     path("api/verify-payment/", verify_payment),
 ]
